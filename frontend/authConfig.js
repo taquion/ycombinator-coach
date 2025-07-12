f8434c4bf83137ad94b610ed4a6dcd44280950e7
taquion <dnader90@gmail.com>
2025-07-12 11:34:16 -0600

    feat: update redirect URI to custom domain

--- frontend/authConfig.js
+++ frontend/authConfig.js
@@ -3,7 +3,7 @@ const msalConfig = {
         clientId: "1f9e42ba-8de3-42a2-ab9f-0f72ac5474c0",
         authority: "https://ycoachapp.ciamlogin.com/ycoachapp.onmicrosoft.com/", // Correct authority based on working endpoint
         knownAuthorities: ["ycoachapp.ciamlogin.com"],
-        redirectUri: "https://polite-plant-0f3f4a41e.2.azurestaticapps.net/", // Match the working redirect URI from Azure test
+        redirectUri: "https://www.ycombinator.coach/", // Point to the custom domain // Match the working redirect URI from Azure test
     },
     cache: {
         cacheLocation: "sessionStorage",
